8132a6e1bac792077e16abed1227c6f19a18e3a6
Dmytro Butmerchuk <dmytro.butmerchuk@globallogic.com>
2022-08-17 18:56:53 +0300

    body output added

--- get_request.py
+++ get_request.py
@@ -9,4 +9,16 @@ url = "https://gorest.co.in"
 
 resp = requests.get(f"{url}/public/v2/users", headers=headers)
 
-print(f"Status code: {resp.status_code}\nHeaders: \n{(json.dumps(dict(resp.headers), indent=4))}")
+
+def prettify(s):
+    return s.strip("[]").replace("},", "}  ").split("  ")
+
+ldata = prettify(resp.content.decode())
+
+print(f"Status code: {resp.status_code}\nHeaders: \n{(json.dumps(dict(resp.headers), indent=4))} \nBody: \n")
+
+for i in ldata:
+    print(i)
+
+
+
