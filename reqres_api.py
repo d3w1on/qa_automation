62b151f67abfa52529005781d187d5462f893aea
Dmytro Butmerchuk <dmytro.butmerchuk@globallogic.com>
2022-08-25 18:34:01 +0300

    Comments describing functions parameters were added

--- reqres_api.py
+++ reqres_api.py
@@ -4,21 +4,38 @@ import json
 
 url = "https://reqres.in/api"
 
+
 # headers should be moved out of this file
 # headers = {
 #     "User-Agent": "test"
 # }
 
-
+#######################################
 def register_successful(payload):
     return requests.post(f"{url}/register", data=payload)
 
 
-def get_users_list(page):
-    return requests.get(f"{url}/users?page={page}")
+# Payload Sample below
+
+# payload = {
+#   "email": "eve.holt@reqres.in",
+#   "password": "pistol"
+# }
+
+#######################################
+def get_users_list(params):
+    return requests.get(f"{url}/users", params=params)
 
 
+# Params Sample below
+
+# params = {"page": 1}
+
+#######################################
 def get_single_user(userId):
     return requests.get(f"{url}/users/{userId}")
 
 
+# userId as int value in range 1 - 12
+
+#######################################
